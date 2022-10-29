59fad7a6899d78807327377d61b58341920a4845
bbbp <82454663+bbbp-park@users.noreply.github.com>
2022-10-29 17:27:12 +0900

    그룹 나가기 기능 작업

--- app/src/main/java/com/example/echoloc/PublicGroupChatting.kt
+++ app/src/main/java/com/example/echoloc/PublicGroupChatting.kt
@@ -3,7 +3,15 @@ package com.example.echoloc
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.drawerlayout.widget.DrawerLayout.DrawerListener
@@ -12,10 +20,12 @@ import com.bumptech.glide.Glide
 import com.example.echoloc.adapter.MemberAdapter
 import com.example.echoloc.adapter.PublicChattingAdapter
 import com.example.echoloc.database.Pref
+import com.example.echoloc.model.LocationModel
 import com.example.echoloc.model.MessageModel
 import com.example.echoloc.model.Usermodel
 import com.example.echoloc.util.getDateTime
 import com.example.echoloc.util.showToast
+import com.google.android.gms.maps.model.LatLng
 import com.google.firebase.database.*
 import com.google.firebase.database.ktx.getValue
 import kotlinx.android.synthetic.main.activity_public_group_chatting.*
@@ -28,12 +38,15 @@ class PublicGroupChatting : AppCompatActivity(), View.OnClickListener {
 
     var group_id = ""
     var admin_id : String? = null
+    var admin: Usermodel? = null
     lateinit var pref: Pref
     lateinit var adapter: PublicChattingAdapter
     lateinit var list: ArrayList<MessageModel>
 
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var drawerView: View
+    lateinit var eLatLng: LatLng
+    var myKey: String? = null
 
     lateinit var memberAdapter: MemberAdapter
     var memberList = mutableListOf<Usermodel>()
@@ -57,11 +70,89 @@ class PublicGroupChatting : AppCompatActivity(), View.OnClickListener {
         recyclerview.adapter = adapter
 
         // 네비게이션 바
+
         drawerLayout = findViewById(R.id.drawer_memberList)
         drawerView = findViewById(R.id.drawerView_member)
 
         drawerLayout.setDrawerListener(drawerListener)
         drawerView.setOnTouchListener { v, event -> true }
+
+        rl_admin.setOnClickListener {
+            val mDialogView = LayoutInflater.from(this).inflate(R.layout.activity_profile, null)
+            val mBuilder = AlertDialog.Builder(this)
+            mBuilder.setView(mDialogView)
+
+            val mAlertDialog = mBuilder.show()
+
+            val mUserProfileImg = mDialogView.findViewById<ImageView>(R.id.iv_profileImg)
+            Glide.with(this).load(admin?.profileImageUrl).into(mUserProfileImg)
+
+            val mUserName = mDialogView.findViewById<TextView>(R.id.tv_userName)
+            mUserName.text = admin?.name?.trim()
+
+            val mUserCall = mDialogView.findViewById<TextView>(R.id.tv_userCall)
+            mUserCall.text = admin?.call?.trim()
+
+            database.getReference("Echoloc").child("location").child(group_id)
+                .addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        for (data in snapshot.children) {
+                            val locationModel = data.getValue(LocationModel::class.java)
+                            if (admin_id == locationModel!!.user_id) {
+                                eLatLng = LatLng(locationModel.latitude, locationModel.longitude)
+                                break
+                            }
+                        }
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+                        TODO("Not yet implemented")
+                    }
+
+                })
+            val mNavigation = mDialogView.findViewById<Button>(R.id.btn_navigation)
+            mNavigation.setOnClickListener {
+
+                val intent = Intent(this, GDirectionActivity::class.java)
+                intent.putExtra("sName", pref.getData("name"))
+                intent.putExtra("eName", admin?.name)
+                intent.putExtra("sLat", pref.getData("latitude").toDouble())
+                intent.putExtra("sLon", pref.getData("longitude").toDouble())
+                intent.putExtra("eLat", eLatLng.latitude)
+                intent.putExtra("eLon", eLatLng.longitude)
+                intent.putExtra("group_id", group_id)
+                intent.putExtra("profile", admin?.profileImageUrl)
+                startActivity(intent)
+                mAlertDialog.dismiss()
+            }
+        }
+
+        val mDatabaseReference = database.getReference("Echoloc").child("public").child(group_id).child("members")
+        mDatabaseReference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (data in snapshot.children) {
+                    val userModel = data.getValue(Usermodel::class.java)
+
+                    if (pref.getData("id") == userModel!!.id) {
+                        myKey = data.key.toString()
+                        break
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+        btn_exit.setOnClickListener {
+            mDatabaseReference.child(myKey!!).removeValue()
+            Toast.makeText(this, "그룹을 탈퇴했습니다.", Toast.LENGTH_SHORT).show()
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
 
         database.getReference("Echoloc").child("public").child(group_id).child("roomname")
             .addValueEventListener(object : ValueEventListener{
@@ -136,6 +227,7 @@ class PublicGroupChatting : AppCompatActivity(), View.OnClickListener {
                         val userModel = data.getValue(Usermodel::class.java)
 
                         if (admin_id == userModel!!.id) {
+                            admin = data.getValue(Usermodel::class.java)!!
                             tv_adminName.text = userModel!!.name
                             Glide.with(this@PublicGroupChatting).load(Uri.parse(userModel.profileImageUrl)).into(iv_adminProfile)
                             iv_adminProfile.clipToOutline = true
@@ -183,7 +275,6 @@ class PublicGroupChatting : AppCompatActivity(), View.OnClickListener {
 //                intent.putExtra("group_id", group_id)
 //                intent.putExtra("isfrom_public","1")
 //                startActivity(intent)
-
                 getMemberList()
                 drawerLayout.openDrawer(drawerView)
             }
